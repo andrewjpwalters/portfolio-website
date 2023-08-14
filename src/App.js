bc2bb5ecb307592ebae4129743ad710016a6e179
Andrew Walters <andrewjpwalters@gmail.com>
2023-08-14 04:04:54 -0500

    create Navbar component and import to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from "./Home";
 import Projects from "./Projects";
 import About from "./About";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1>Hello from App!</h1>
       <Home />
       <Projects />
